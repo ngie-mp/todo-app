02044c4c2a4ce2dfd0a804726b40de01ae0888c2
ngie <angeles.morap@gmail.com>
2017-01-08 22:46:08 +0100

    Make two more tests

--- app/js/app-spec.js
+++ app/js/app-spec.js
@@ -1,11 +1,17 @@
 describe('todo list manager app', function() {
+
     it('should add a task on todo list', function(){
        browser.get('http://127.0.0.1:8080');
        element(by.model('newTodo')).sendKeys('write first e2e test');
     });
 
+    it('should delete a task from todo list', function(){
+        element(by.css('.destroy')).click();
+    });
 
-
+    it('should checked all the task', function(){
+        element(by.model('allChecked')).click();
+    });
 
     //Make test execute slowly
     var slowTest = browser.driver.controlFlow().execute;
@@ -15,7 +21,7 @@ describe('todo list manager app', function() {
 
         // set time
         slowTest.call(browser.driver.controlFlow(), function() {
-            return protractor.promise.delayed(500);
+            return protractor.promise.delayed(800);
         });
 
         return slowTest.apply(browser.driver.controlFlow(), args);
